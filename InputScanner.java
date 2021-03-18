9573ba6aa76e692fe05d007b39c7d240b1ccbee4
bhanubankoti <bhanubankoti@users.noreply.github.com>
2021-03-18 09:51:57 +0530

    Add files via upload

---
+++ InputScanner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class InputScanner{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String s = sc.next();
+
+        System.out.print("a : " +a+" s : "+s);
+    }
+}
