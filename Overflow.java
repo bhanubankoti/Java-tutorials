857cc08bf6e4df14d05942868b84076a418ef09e
bhanubankoti <bhanubankoti@users.noreply.github.com>
2021-03-18 08:26:41 +0530

    Add files via upload

---
+++ Overflow.java
@@ -0,0 +1,11 @@
+public class Overflow {
+    public static void main(String[] args) {
+        int a ;
+        byte b;
+        a = 146;
+        b = (byte)a;
+
+        System.out.println(b);
+
+    }
+}
